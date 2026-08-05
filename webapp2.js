312e2528793f5d58c52116041b84aba499db652a
jennabean7 <163203770+jennabean7@users.noreply.github.com>
2026-08-04 23:20:01 -0400

    Add files via upload

--- webapp2.js
+++ webapp2.js
@@ -6,52 +6,51 @@ const fs = require('fs')
 const MongoClient = require('mongodb').MongoClient;
 const mongoURL = "mongodb://jbean03_db_user:4JFxS5zGtTYVwRDA@ac-kcfncmb-shard-00-00.aedupsh.mongodb.net:27017,ac-kcfncmb-shard-00-01.aedupsh.mongodb.net:27017,ac-kcfncmb-shard-00-02.aedupsh.mongodb.net:27017/?ssl=true&replicaSet=atlas-zr2gwz-shard-0&authSource=admin&appName=assignment10"
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  urlObj = url.parse(req.url,true)
-  if (urlObj.pathname == "/") 
-  {
-     res.write("<h2>Zip Code Lookup</h2>");
-     s = "<form method='get' action='/process'>" +
-         "Enter a town name or zip code: <input type='text' name='id'><br /><input type='submit'></form>"
-     res.write(s)
-     res.end()
-  }
-  else if (urlObj.pathname == "/process") {
-    id = urlObj.query.id
-
-    if(isNaN(id[0])){
-        MongoClient.connect(mongoURL, function(err, db){
-            if(err){
-                return console.log(err);
-            }
-            var dbo = db.db("assignment10");
-            result = dbo.collection('places').find({town: id}).toArray(function(err, item){
-                if(err){
-                    return console.log(err);
-                }
-                console.log(JSON.stringify(item))
-                res.write(JSON.stringify(item))
-                res.end()
-                db.close()
-            })
-        })
-    } else{
-        MongoClient.connect(mongoURL, function(err, db){
-            if(err){
-                return console.log(err);
-            }
-            var dbo = db.db("assignment10");
-            result = dbo.collection('places').find({zips: {$all: [id]}}).toArray(function(err, item){
-                if(err){
-                    return console.log(err);
-                }
-                console.log(JSON.stringify(item))
-                res.write(JSON.stringify(item))
-                res.end()
-                db.close()
-            })
-        })
+MongoClient.connect(mongoURL, function(err, db){
+    if(err){
+        return console.log(err);
     }
-  }
-}).listen(port);
+    var dbo = db.db("assignment10");
+    var collection = dbo.collection('places');
+
+    http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        urlObj = url.parse(req.url,true)
+        if (urlObj.pathname == "/") 
+        {
+            res.write("<h2>Zip Code Lookup</h2>");
+            s = "<form method='get' action='/process'>" +
+                "Enter a town name or zip code: <input type='text' name='id'><br /><input type='submit'></form>"
+            res.write(s)
+            res.end()
+        }
+        else if (urlObj.pathname == "/process") {
+            id = urlObj.query.id
+
+            if(isNaN(id[0])){
+                var dbo = db.db("assignment10");
+                result = dbo.collection('places').find({town: id}).toArray(function(err, item){
+                    if(err){
+                        return console.log(err);
+                    }
+                    console.log(JSON.stringify(item))
+                    res.write(JSON.stringify(item))
+                    res.end()
+                    db.close()
+                })
+            } else{
+                var dbo = db.db("assignment10");
+                result = dbo.collection('places').find({zips: {$all: [id]}}).toArray(function(err, item){
+                    if(err){
+                        return console.log(err);
+                    }
+                    console.log(JSON.stringify(item))
+                    res.write(JSON.stringify(item))
+                    res.end()
+                    db.close()
+                })
+            }
+        }
+    }).listen(port);
+    db.close();
+})
